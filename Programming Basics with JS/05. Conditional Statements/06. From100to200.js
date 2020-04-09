def834ff34571ff4931d378b6d52817cb7186f3f
Plamen Shumanski <plamen1030@abv.bg>
2020-04-09 14:50:20 +0300

    Create 06. From100to200.js

---
+++ Programming Basics with JS/05. Conditional Statements/06. From100to200.js
@@ -0,0 +1,15 @@
+function solve(input) {
+
+    let num = Number(input.shift());
+
+    if (num < 100) {
+        console.log("Less than 100");
+    } else if (num <= 200) {
+        console.log("Between 100 and 200");
+    } else {
+        console.log("Greater than 200");
+    }
+
+}
+
+solve([400]);
