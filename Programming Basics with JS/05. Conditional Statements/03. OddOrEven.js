0cf57797bb543b83d3ae1b19f706af1af4166273
Plamen Shumanski <plamen1030@abv.bg>
2020-04-09 14:46:11 +0300

    Create 03. OddOrEven.js

---
+++ Programming Basics with JS/05. Conditional Statements/03. OddOrEven.js
@@ -0,0 +1,13 @@
+function solve(input) {
+
+    let num = Number(input.shift());
+    if (num % 2 == 0) {
+        console.log("even");
+    }
+    else {
+        console.log("odd");
+    }
+
+}
+
+solve([1024]);
