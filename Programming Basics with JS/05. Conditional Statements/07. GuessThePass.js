fb41e0a731ecd38d9a5f5eaa47950591b4ae98ad
Plamen Shumanski <plamen1030@abv.bg>
2020-04-09 14:51:32 +0300

    Create 07. GuessThePass.js

---
+++ Programming Basics with JS/05. Conditional Statements/07. GuessThePass.js
@@ -0,0 +1,12 @@
+function solve(input) {
+
+    let str = input.shift();
+    if (str === "s3cr3t!P@ssw0rd") {
+        console.log("Welcome");
+    } else {
+        console.log("Wrong password!")
+    }
+
+}
+
+solve(["s3cs"]);
