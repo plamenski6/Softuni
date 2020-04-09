200460f8384e8cb4eb4d01bc4f7d11a3d3d90f87
Plamen Shumanski <plamen1030@abv.bg>
2020-04-09 14:45:13 +0300

    Create 02. GreaterNumber.js

---
+++ Programming Basics with JS/05. Conditional Statements/02. GreaterNumber.js
@@ -0,0 +1,15 @@
+function solve(input) {
+
+    let a = Number(input.shift());
+    let b = Number(input.shift());
+
+    if(a > b){
+        console.log(a);
+    }
+    else{
+        console.log(b);
+    }
+
+}
+
+solve([3,2]);
