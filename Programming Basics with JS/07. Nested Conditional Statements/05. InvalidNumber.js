c1b07c849f702d3cee295a6218b8d19ef95490c4
Plamen Shumanski <plamen1030@abv.bg>
2020-04-09 15:29:22 +0300

    Create 05. InvalidNumber.js

---
+++ Programming Basics with JS/07. Nested Conditional Statements/05. InvalidNumber.js
@@ -0,0 +1,13 @@
+function solve(input) {
+
+    let n = Number(input.shift());
+
+    if ((n >= 100 && n <= 200) || n == 0) {
+
+    } else {
+        console.log("invalid");
+    }
+
+}
+
+solve([75]);
