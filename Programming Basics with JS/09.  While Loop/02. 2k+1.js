4b392552a4cef1bff36c1e8b425e1698724a67d5
Plamen Shumanski <plamen1030@abv.bg>
2020-04-13 13:49:06 +0300

    Create 02. 2k+1.js

---
+++ Programming Basics with JS/09.  While Loop/02. 2k+1.js
@@ -0,0 +1,11 @@
+function solve(input) {
+
+    let n = Number(input.shift());
+    let output = 1;
+    while (output <= n) {
+        console.log(output);
+        output = output * 2 + 1;
+    }
+}
+
+solve([8]);
