b15858049344554d047ee174267478f51c3ec7a6
Plamen Shumanski <plamen1030@abv.bg>
2020-04-13 13:50:33 +0300

    Create 03. Account Balance.js

---
+++ Programming Basics with JS/09.  While Loop/03. Account Balance.js
@@ -0,0 +1,25 @@
+function solve(input) {
+
+    let n = Number(input.shift());
+    let total = 0;
+    let counter = 0;
+
+    while (n > counter) {
+        let money = Number(input.shift());
+        if (money < 0) {
+            console.log("Invalid operation!")
+            break;
+        }
+        console.log(`Increase: ${money.toFixed(2)}`);
+        total = total + money;
+        counter++;
+    }
+    console.log(`Total: ${total.toFixed(2)}`);
+
+}
+
+solve([5,
+    120,
+    45.55,
+    -150
+]);
