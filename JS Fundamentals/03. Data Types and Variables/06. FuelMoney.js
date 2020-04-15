e12786030388969c772ae516f57ea93f52216c62
Plamen Shumanski <plamen1030@abv.bg>
2020-04-15 14:48:16 +0300

    Create 06. FuelMoney.js

---
+++ JS Fundamentals/03. Data Types and Variables/06. FuelMoney.js
@@ -0,0 +1,10 @@
+function solve(distance, passengers, price) {
+
+    let fuel = (distance / 100) * 7;
+    fuel += passengers * 0.1;
+    let money = price * fuel
+    console.log(`Needed money for that trip is ${money}lv.`)
+
+}
+
+solve(260, 9, 2.49);
