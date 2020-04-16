b68216e273b791002b4feda9b435880c68c84209
Plamen Shumanski <plamen1030@abv.bg>
2020-04-16 15:33:25 +0300

    Create 06. BombNumbers.js

---
+++ JS Fundamentals/11. Exercise - Arrays Advanced/06. BombNumbers.js
@@ -0,0 +1,20 @@
+function bomb(numbers, params) {
+    let [target, power] = params;
+
+    for (let i = 0; i < numbers.length; i++) {
+        let currentNum = numbers[i];
+
+        if (currentNum === target) {
+            let startIndex = Math.max(0, i - power);
+            let countToEnd = power * 2 + 1;
+
+            numbers.splice(startIndex, countToEnd);
+            i = i - power - 1;
+        }
+    }
+
+    console.log(numbers.reduce((a, b) => a + b, 0));
+
+}
+
+bomb([1, 4, 1, 1, 1, 1, 1, 4, 1], [4, 2]);
