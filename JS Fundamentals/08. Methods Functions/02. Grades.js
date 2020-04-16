19ac29c42fdcee38c082dec9aef6b712655b8a13
Plamen Shumanski <plamen1030@abv.bg>
2020-04-16 14:49:53 +0300

    Update 02. Grades.js

--- JS Fundamentals/08. Methods Functions/02. Grades.js
+++ JS Fundamentals/08. Methods Functions/02. Grades.js
@@ -1,7 +1,17 @@
-function solve(num, pow) {
+function solve(n) {
 
-    return Math.pow(num, pow);
+    if (n >= 2 && n <= 2.99) {
+        return 'Fail';
+    } else if (n >= 3 && n <= 3.49) {
+        return 'Poor';
+    } else if (n >= 3.5 && n <= 4.49) {
+        return 'Good';
+    } else if (n >= 4.5 && n <= 5.49) {
+        return 'Very good'
+    } else if (n >= 5.5 && n <= 6) {
+        return 'Excellent'
+    }
 
 }
 
-console.log(solve(2, 8));
+console.log(solve(5));
