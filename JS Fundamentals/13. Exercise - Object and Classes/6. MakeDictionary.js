a9e645dec1c345de06e0980d8b53497e16442b4c
Plamen Shumanski <plamen1030@abv.bg>
2020-04-18 13:27:06 +0300

    Create 6. MakeDictionary.js

---
+++ JS Fundamentals/13. Exercise - Object and Classes/6. MakeDictionary.js
@@ -0,0 +1,20 @@
+function solve(arr) {
+
+    let obj = {};
+
+    for (let terms of arr) {
+        let newTerms = JSON.parse(terms);
+        obj = Object.assign(newTerms);
+    }
+
+    console.log(obj)
+
+}
+
+solve([
+    '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+    '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+    '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+    '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+    '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
+]);
