dce8fbf85e1e137336fdff74f58e25466685e374
Plamen Shumanski <plamen1030@abv.bg>
2020-04-20 17:01:57 +0300

    Update Concert.js

--- JS Fundamentals/21. Final Exams/Exam Preparation 1/Concert/Concert.js
+++ JS Fundamentals/21. Final Exams/Exam Preparation 1/Concert/Concert.js
@@ -1,34 +1,34 @@
-function solve(input){
+function solve(input) {
 
     let bandName = input.pop();
     let obj = {};
     let totalTime = 0;
-    for(let i of input){
+    for (let i of input) {
 
-        if(i === "start of concert"){
+        if (i === "start of concert") {
             break;
         }
 
         let cmd = i.split("; ");
-        
-        switch(cmd[0]){
+
+        switch (cmd[0]) {
             case "Add":
-                if(!obj.hasOwnProperty(cmd[1])){
+                if (!obj.hasOwnProperty(cmd[1])) {
                     obj[cmd[1]] = {
                         add: cmd[2],
                         play: 0
                     };
                 } else {
                     let members = cmd[2].split(", ");
-                    for(let j of members){
-                        if(!obj[cmd[1]].add.includes(j)){
-                           obj[cmd[1]].add += `, ${j}`;
+                    for (let j of members) {
+                        if (!obj[cmd[1]].add.includes(j)) {
+                            obj[cmd[1]].add += `, ${j}`;
                         }
                     }
                 }
                 break;
             case "Play":
-                if(!obj.hasOwnProperty(cmd[1])){
+                if (!obj.hasOwnProperty(cmd[1])) {
                     obj[cmd[1]] = {
                         add: "",
                         play: Number(cmd[2])
@@ -42,22 +42,23 @@ function solve(input){
     }
 
     let arr = Object.entries(obj);
-    arr.sort((a,b) => b[1].play - a[1].play || a[0].localeCompare(b[0]));
-    
+    arr.sort((a, b) => b[1].play - a[1].play || a[0].localeCompare(b[0]));
+
     console.log(`Total time: ${totalTime}`)
     arr.forEach(el => {
         console.log(`${el[0]} -> ${el[1].play}`);
     });
-    
-    for(let k of arr){
-        if(k[0] === bandName){
+
+    for (let k of arr) {
+        if (k[0] === bandName) {
             console.log(bandName)
             let members = k[1].add.split(", ");
-            for(let person of members){
+            for (let person of members) {
                 console.log(`=> ${person}`);
             }
         }
     }
+    
 }
 
 solve([
@@ -68,4 +69,4 @@ solve([
     'Play; The Beatles; 3828',
     'start of concert',
     'The Beatles'
-  ])
+]);
