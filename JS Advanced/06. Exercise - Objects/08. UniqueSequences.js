b341a65eba508986791083dd5f2bbe031cb5aaee
Plamen Shumanski <plamen1030@abv.bg>
2020-04-22 15:18:56 +0300

    Create 08. UniqueSequences.js

---
+++ JS Advanced/06. Exercise - Objects/08. UniqueSequences.js
@@ -0,0 +1,37 @@
+function solve(jsonarray) {
+
+    let array = [];
+
+    for (let str of jsonarray) {
+        let arr = JSON.parse(str);
+        arr.sort((a, b) => b - a);
+        if (!exists(array, arr))
+            array.push(arr);
+        //console.log(arr);
+    }
+    array.sort((a, b) => a.length - b.length);
+    array.forEach(a => console.log('[' + a.join(', ') + ']'));
+
+    function exists(arr, subarr) {
+        for (let a of arr) {
+            if (equalArray(a, subarr))
+                return true;
+        }
+        return false;
+    }
+
+    function equalArray(arr1, arr2) {
+        if (arr1.length == arr2.length) {
+            for (let i = 0; i < arr1.length; i++)
+                if (arr1[i] != arr2[i])
+                    return false
+            return true;
+        }
+        return false;
+    }
+
+}
+
+solve(["[-3, -2, -1, 0, 1, 2, 3, 4]",
+    "[10, 1, -17, 0, 2, 13]",
+    "[4, -3, 3, -2, 2, -1, 1, 0]"]);
