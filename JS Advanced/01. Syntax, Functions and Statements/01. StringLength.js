003f32a05c0a311ef175795ffbd1dca69d461e6a
Plamen Shumanski <plamen1030@abv.bg>
2020-04-22 13:11:37 +0300

    Create 01. StringLength.js

---
+++ JS Advanced/01. Syntax, Functions and Statements/01. StringLength.js
@@ -0,0 +1,12 @@
+function solve(a, b, c) {
+
+    let sum1 = a.length;
+    let sum2 = b.length;
+    let sum3 = c.length;
+
+    console.log(sum1 + sum2 + sum3);
+    console.log(Math.floor((sum1 + sum2 + sum3) / 3));
+
+}
+
+solve('chocolate', 'ice cream', 'cake');
