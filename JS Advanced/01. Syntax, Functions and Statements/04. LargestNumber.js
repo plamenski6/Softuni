6e682044bab78f4fdb85e2c215c8e6f2d43df411
Plamen Shumanski <plamen1030@abv.bg>
2020-04-22 13:32:42 +0300

    Create 04. LargestNumber.js

---
+++ JS Advanced/01. Syntax, Functions and Statements/04. LargestNumber.js
@@ -0,0 +1,13 @@
+function solve(a, b, c) {
+
+    if (a > b && a > c) {
+        console.log(`The largest number is ${a}.`);
+    } else if (b > a && b > c) {
+        console.log(`The largest number is ${b}.`);
+    } else if (c > a && c > b) {
+        console.log(`The largest number is ${c}.`);
+    }
+
+}
+
+solve(5, -3, 16);
