11a49b34ceac20d77003b9e4166152791f84db5e
Plamen Shumanski <plamen1030@abv.bg>
2020-04-22 13:55:36 +0300

    Create 01. SumFirstLast.js

---
+++ JS Advanced/03. Arrays/01. SumFirstLast.js
@@ -0,0 +1,10 @@
+function solve(arr) {
+
+    let first = arr[0];
+    let last = arr[arr.length - 1];
+    let result = Number(first) + Number(last);
+    console.log(result);
+
+}
+
+solve(['20', '30', '40']);
