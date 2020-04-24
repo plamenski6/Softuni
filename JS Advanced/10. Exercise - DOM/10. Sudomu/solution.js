1dad2bc7a945ed9f91075631e99ec9059c1a8bdb
Plamen Shumanski <plamen1030@abv.bg>
2020-04-24 14:59:49 +0300

    Need to be done...

---
+++ JS Advanced/10. Exercise - DOM/10. Sudomu/solution.js
@@ -0,0 +1,10 @@
+function solve() {
+
+    let buttons = document.querySelectorAll("#exercise, button");
+    buttons[0].addEventListener('click', (e) => {
+
+    let cells = Array.from(document.querySelectorAll("tbody input")).map(e => e.value);
+        
+    });
+    
+}
