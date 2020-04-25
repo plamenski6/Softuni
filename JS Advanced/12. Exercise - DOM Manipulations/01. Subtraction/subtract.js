e1ea35bad1fefd94ed9e0f4af653b215eadaad5d
Plamen Shumanski <plamen1030@abv.bg>
2020-04-25 16:06:18 +0300

    Create subtract.js

---
+++ JS Advanced/12. Exercise - DOM Manipulations/01. Subtraction/subtract.js
@@ -0,0 +1,9 @@
+function subtract() {
+    
+    let input1 = document.getElementById("firstNumber");
+    let input2 = document.getElementById("secondNumber");
+    let result = document.getElementById("result");
+
+    result.textContent = input1.value - input2.value;
+
+}
