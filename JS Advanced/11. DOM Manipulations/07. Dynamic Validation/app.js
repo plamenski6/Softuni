7788e275a8d4595e979ea3cf9e41df3c8d9e5907
Plamen Shumanski <plamen1030@abv.bg>
2020-04-25 15:59:52 +0300

    Create app.js

---
+++ JS Advanced/11. DOM Manipulations/07. Dynamic Validation/app.js
@@ -0,0 +1,16 @@
+function validate() {
+
+    let inputBox = document.getElementById('email');
+    inputBox.addEventListener('change', performValidation);
+    let pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+    function performValidation() {
+        let email = this.value;
+        if (pattern.test(email)) {
+            this.classList.remove('error');
+        } else {
+            this.classList.add('error');
+        }
+    }
+
+}
