024131806a1dc06561037d657bb12fc33b7fe586
Plamen Shumanski <plamen1030@abv.bg>
2020-04-25 16:13:13 +0300

    Create accordion.js

---
+++ JS Advanced/12. Exercise - DOM Manipulations/03. Accordion/accordion.js
@@ -0,0 +1,14 @@
+function toggle() {
+
+    let button = document.getElementsByClassName("button")[0];
+    let div = document.getElementById("extra");
+
+    if (button.textContent === "More") {
+        div.style.display = "block";
+        button.textContent = "Less"
+    } else {
+        div.style.display = "none";
+        button.textContent = "More"
+    }
+
+}
