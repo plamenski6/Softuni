186917388a9f91a282131a2715c641385acb9cdb
Plamen Shumanski <plamen1030@abv.bg>
2020-04-25 15:48:54 +0300

    Create stopwatch.js

---
+++ JS Advanced/11. DOM Manipulations/04. Stopwatch/stopwatch.js
@@ -0,0 +1,19 @@
+function stopwatch() {
+
+     let start = document.getElementById("startBtn");
+     let stop = document.getElementById("stopBtn");
+     let time = document.getElementById("time");
+
+     start.addEventListener("click", handler);
+
+     function handler(e) {
+          time.textContent = setInterval()
+     }
+
+     stop.addEventListener("click", handler2);
+
+     function handler2(e) {
+
+     }
+
+}
