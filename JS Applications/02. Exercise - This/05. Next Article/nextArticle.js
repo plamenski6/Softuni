1a8aab29fc9d4b6ed10e05452190ebf89d5cacf4
Plamen Shumanski <plamen1030@abv.bg>
2020-04-29 20:56:13 +0300

    Create nextArticle.js

---
+++ JS Applications/02. Exercise - This/05. Next Article/nextArticle.js
@@ -0,0 +1,15 @@
+function getArticleGenerator(articles) {
+
+    const mainDiv = document.getElementById('content');
+    let index = 0;
+    
+    return function(){
+        if(index < articles.length){
+            const article = document.createElement('article');
+            article.textContent = articles[index];
+            mainDiv.appendChild(article);
+            index++;
+        }
+    }
+    
+}
