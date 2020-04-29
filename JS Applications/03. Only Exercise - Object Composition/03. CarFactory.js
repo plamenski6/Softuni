0570115d5e7134912c008577c2284af8cc1a282f
Plamen Shumanski <plamen1030@abv.bg>
2020-04-29 21:04:13 +0300

    Create 03. CarFactory.js

---
+++ JS Applications/03. Only Exercise - Object Composition/03. CarFactory.js
@@ -0,0 +1,16 @@
+function solve(order) {
+
+    let engines = [
+        { power: 90, volume: 1800 },
+        { power: 120, volume: 2400 },
+        { power: 200, volume: 3500 }
+    ];
+
+    return {
+        model: order.model,
+        engine: engines.find(e => order.power <= e.power),
+        carriage: { type: order.carriage, color: order.color },
+        wheels: Array(4).fill(order.wheelsize % 2 === 0 ? order.wheelsize - 1 : order.wheelsize)
+    }
+    
+}
