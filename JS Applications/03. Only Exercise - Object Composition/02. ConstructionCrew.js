a64aa02ba023f0231b920ab90c630f0b62bb4693
Plamen Shumanski <plamen1030@abv.bg>
2020-04-29 21:03:43 +0300

    Create 02. ConstructionCrew.js

---
+++ JS Applications/03. Only Exercise - Object Composition/02. ConstructionCrew.js
@@ -0,0 +1,10 @@
+function solve(worker) {
+
+    if (worker.dizziness) {
+        worker.levelOfHydrated += worker.weight * (worker.experience * 0.1);
+        worker.dizziness = false;
+    }
+
+    return worker;
+    
+}
